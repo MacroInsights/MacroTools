4eb88b43015450a22a50544026c1adbb4f6851f1
Alfredo Romero <Alfredo_Romero@jec.senate.gov>
2023-10-15 19:49:34 -0400

    A function to transform data from xts to tsibble.

---
+++ R/transform_to_tsibble.R
@@ -0,0 +1,19 @@
+#' It transforms an xts object to a tsibble
+#'
+#' @param xts_obj The xts object to transform
+#'
+#' @return A tsibble
+#' @export
+#'
+#' @examples
+#' data_tsibble <- transform_to_tsibble()
+transform_to_tsibble <- function(data = xts_obj) {
+  # Convert xts to tibble
+  tibble_obj <- tidyr::tibble(date = zoo::index(data),
+                              as.data.frame(zoo::coredata(data)))
+
+  # Convert tibble to tsibble
+  tsibble_obj <- tsibble::as_tsibble(tibble_obj, index = date)
+
+  return(tsibble_obj)
+}
