399eda379573b762ca19ac7fb5a21d5bfa23b9e4
Alfredo Romero <Alfredo_Romero@jec.senate.gov>
2023-10-17 14:53:07 -0400

    Adding tcltk to DESCRIPTION

--- R/create_states_clickable_map.R
+++ R/create_states_clickable_map.R
@@ -15,7 +15,7 @@ create_states_clickable_map <- function(
 
   # Loading pacman, libraries from CRAN, libraries from Github
   if (!require("pacman")) install.packages("pacman")
-  pacman::p_load('tidyverse','fredr','cdlTools','leaflet')
+  pacman::p_load('tidyverse','fredr','cdlTools','leaflet','tcltk')
   pacman::p_load_gh('hrbrmstr/albersusa')
 
   # Setting FRED API Key
