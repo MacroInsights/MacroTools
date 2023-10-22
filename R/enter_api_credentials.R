220ae25d08986da2ca874dcfe11e7fbef88a3aa9
Alfredo Romero <Alfredo_Romero@jec.senate.gov>
2023-10-22 12:26:40 -0400

    Adding the enter_api_credentials function.

--- R/enter_api_credentials.R
+++ R/enter_api_credentials.R
@@ -1,3 +1,13 @@
+#' A script to enter API keys for the JECTools package
+#'
+#' @param fred_key An API key from FRED
+#' @param bls_key  An API key from BLS
+#'
+#' @return It creates the API keys in the global environment. Maybe not the best practice
+#' @export
+#'
+#' @examples
+#' enter_api_credentials()
 enter_api_credentials <- function(
     fred_key = readline(prompt = "Enter FRED API key:"),
     bls_key  = readline(prompt = "Enter BLS API key:")) {
