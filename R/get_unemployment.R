fe66b9fc4adf4f9d6e8a1ef79fb7a802252ce1cb
Alfredo Romero <Alfredo_Romero@jec.senate.gov>
2024-02-15 16:33:14 -0500

    Fixing unemployment

--- R/get_unemployment.R
+++ R/get_unemployment.R
@@ -13,7 +13,8 @@
 #' @examples
 #' unemployment <- get_unemployment()
 get_unemployment <- memoise::memoise(function(
-    years = 5,
+    start_year = NULL,
+    end_year = NULL,
     geography = "National",
     demography = FALSE,
     latest = FALSE,
@@ -21,6 +22,30 @@ get_unemployment <- memoise::memoise(function(
     BLS_key = blsKey)
 {
 
+  #######################################################################
+  #                          LOGIC FOR DATES
+  # Current year
+  currentYear <- as.numeric(format(Sys.Date(), "%Y"))
+
+  # If end_year is NULL, set it to the current year
+  if (is.null(end_year)) {
+    end_year <- currentYear
+  } else {
+    end_year <- as.numeric(end_year) # Ensure end_year is numeric
+  }
+
+  # If start_year is NULL, set it to five years less than end_year
+  if (is.null(start_year)) {
+    start_year <- end_year - 5
+  } else {
+    start_year <- as.numeric(start_year) }
+
+
+  # Ensure start_year is less than end_year
+  if (start_year >= end_year) {
+    stop("start_year must be less than end_year")
+  }
+  #######################################################################
 
   # Setting FRED API Key
   fred_key <- gsub("\"", "", fred_key)
@@ -64,11 +89,9 @@ get_unemployment <- memoise::memoise(function(
   # Paparameters of the request
   params <- list(
     series_id = variables,
-    observation_start = rep(as.Date(lubridate::floor_date(Sys.Date(),
-                                               unit = "weeks") + lubridate::days(7))-lubridate::years(years),
+    observation_start = rep(as.Date(paste0(start_year,"-01","-01")),
                             num_series),
-    observation_end = rep(as.Date(lubridate::floor_date(Sys.Date(),
-                                             unit = "weeks") + lubridate::days(7)), num_series))
+    observation_end = rep(as.Date(paste0(end_year,"-12","-31")), num_series))
 
   # Actual request of data
   raw_data_fred <- purrr::pmap_dfr(
